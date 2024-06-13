34ca97e70d3472e9a16baf2831baf3093c3d5899
InterstellarOne <63889949+InterstellarOne@users.noreply.github.com>
2024-06-13 10:29:18 -0700

    Update LRO-installer-1.5.0.7a.sh
    Change icon

--- LRO-installer-1.5.0.7a.sh
+++ LRO-installer-1.5.0.7a.sh
@@ -3,7 +3,7 @@
 mkdir ~/.local/share/lroverhaul
 cd ~/.local/share/lroverhaul
 wget https://github.com/LunaKampling/LROverhaul/files/15278421/1.5.0.7a.zip
-wget -nc https://raw.githubusercontent.com/InterstellarOne/lro-temp/main/icon.png
+wget -O https://raw.githubusercontent.com/LunaKampling/LROverhaul/main/src/Resources/icon.ico
 unzip -o 1.5.0.7a.zip
 rm 1.5.0.7a.zip
 cd ~/.local/share/applications/
