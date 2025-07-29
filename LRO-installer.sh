98d648fd3e25a2e40223f5ceb81f129c44bd62a6
InterstellarOne <63889949+InterstellarOne@users.noreply.github.com>
2025-07-29 13:03:02 -0700

    Update LRO-installer.sh

--- LRO-installer.sh
+++ LRO-installer.sh
@@ -10,4 +10,4 @@ cd ~/.local/share/applications/
 wget -nc https://raw.githubusercontent.com/InterstellarOne/lro-linux-installer/main/lroverhaul.desktop
 sed -i "s/*/$USER/g" lroverhaul.desktop
 echo Line Rider Overhaul installed!
-mono ~/.local/share/lroverhaul/linerider.exe
+dotnet ~/.local/share/lroverhaul/linerider.dll
