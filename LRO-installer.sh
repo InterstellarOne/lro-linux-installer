3c1adf6754ac2d3f5bc31f742fc0568bce439fac
InterstellarOne <63889949+InterstellarOne@users.noreply.github.com>
2025-07-29 12:40:08 -0700

    Update LRO-installer.sh

--- LRO-installer.sh
+++ LRO-installer.sh
@@ -3,9 +3,9 @@
 mkdir ~/.local/share/lroverhaul
 cd ~/.local/share/lroverhaul
 wget https://github.com/LunaKampling/LROverhaul/releases/download/1.5.0.8a/1.5.0.8-crossplatform.zip
-wget -O https://raw.githubusercontent.com/InterstellarOne/lro-linux-installer/refs/heads/main/icon.png
-unzip -o 1.5.0.8a.zip
-rm 1.5.0.8a.zip
+wget https://raw.githubusercontent.com/InterstellarOne/lro-linux-installer/refs/heads/main/icon.png
+unzip -o 1.5.0.8a-crossplatform.zip
+rm 1.5.0.8a-crossplatform.zip
 cd ~/.local/share/applications/
 wget -nc https://raw.githubusercontent.com/InterstellarOne/lro-temp/main/lroverhaul.desktop
 sed -i "s/*/$USER/g" lroverhaul.desktop
